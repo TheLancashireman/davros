0bb06984188e9e978a3572365f61d793a5fda63e
David Haworth <dh@thelancashireman.org>
2017-12-28 15:13:01 +0100

    Remove stuff accidentally merged from wrong repository.

--- code/goodbye.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int unused_argc, char **unused_argv)
-{
-	printf("Goodbye, cruel world.\n");
-	return 0;
-}
--- code/hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int unused_argc, char **unused_argv)
-{
-	printf("Hello world.\n");
-	return 0;
-}
