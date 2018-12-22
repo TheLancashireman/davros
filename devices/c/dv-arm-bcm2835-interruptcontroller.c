64710c02d1c91302e263ff5b94c9405063613349
David Haworth <dh@thelancashireman.org>
2018-12-22 15:39:19 +0100

    Intermediate commit: dv-arm-bcm2835-interruptcontroller.c, before forking it for davros-3 and davroska

--- devices/c/dv-arm-bcm2835-interruptcontroller.c
+++ devices/c/dv-arm-bcm2835-interruptcontroller.c
@@ -25,38 +25,15 @@
 #include <cpufamily/arm/h/dv-arm-dispatch.h>
 #endif
 
-/* dv_bcm2835_interrupt_handler() - interrupt handler (BCM2835 version)
- *
- * The BCM2835 interrupt controller is a very simple unit. There is no vectoring, and
- * there are no priority levels.
- *
- * The first step in this function is to read the pending registers into an array.
- * This gives us a way of configuring IRQs and handling them;
- * Each configured IRQ has an index and a mask. When the software accepts the
- * interrupt it clears the pending bit in the array and disables the interrupt.
- *
- * After processing all the known IRQs, if there are any left they are reported as unknown
- * and disabled.
- *
- * dv_bcm2835_irq_list[] is a table of all the interrupt sources that can be used.
+/* dv_bcm2835_irq_list[] is a table of all the interrupt sources that can be used.
  * The index in this list gives the vector number of the IRQ.
- * The list is minimal at the moment but can be extended later.
- *
- * Interrupts are handled by calling the function from the corresponding
- * entry in the software vector table. The standard interrupt dispatcher cannot be used because
- * it is designed to handle a single interrupt, whereas we must handle every interrupt that's
- * pending. Before calling the handler the source is disabled. This means that if the handler
- * must re-enable the interrupt if it has been completely handled within the kernel. If the
- * handler spawns an "ISR" thread, the kernel must re-enable the interrupt source
- * when the ISR terminates.
+ * The list must be ordered so that the register numbers are clustered in order 0,1,2.
 */
-
 const dv_bcm2835_irq_t dv_bcm2835_irq_list[dv_n_bcm2835_iid] =
 {
 	[dv_iid_syst_cm1]	= {	0,	DV_INT_SYST_CM1		},
 	[dv_iid_syst_cm3]	= {	0,	DV_INT_SYST_CM3		},
 	[dv_iid_aux]		= {	0,	DV_INT_AUX			},
-#if 0
 	[dv_iid_i2cspi_slv]	= {	1,	DV_INT_I2CSPI_SLV	},
 	[dv_iid_pwa0]		= {	1,	DV_INT_PWA0			},
 	[dv_iid_pwa1]		= {	1,	DV_INT_PWA1			},
@@ -69,8 +46,6 @@ const dv_bcm2835_irq_t dv_bcm2835_irq_list[dv_n_bcm2835_iid] =
 	[dv_iid_spi]		= {	1,	DV_INT_SPI			},
 	[dv_iid_pcm]		= {	1,	DV_INT_PCM			},
 	[dv_iid_uart]		= {	1,	DV_INT_UART			},
-#endif
-#if 0
 	[dv_iid_timer]		= {	2,	DV_INT_TIMER		},
 	[dv_iid_mailbox]	= {	2,	DV_INT_MAILBOX		},
 	[dv_iid_doorbell0]	= {	2,	DV_INT_DOORBELL0	},
@@ -79,45 +54,66 @@ const dv_bcm2835_irq_t dv_bcm2835_irq_list[dv_n_bcm2835_iid] =
 	[dv_iid_gpu1halt]	= {	2,	DV_INT_GPU1HALT		},
 	[dv_iid_illegal0]	= {	2,	DV_INT_ILLEGAL0		},
 	[dv_iid_illegal1]	= {	2,	DV_INT_ILLEGAL1		},
-#endif
 };
 
-static const dv_u32_t known_irq[3] =
-{
-	DV_INT_SYST_CM1 | DV_INT_SYST_CM3 | DV_INT_AUX,
-	DV_INT_I2CSPI_SLV | DV_INT_PWA0 | DV_INT_PWA1 | DV_INT_SMI |
-		DV_INT_GPIO0 | DV_INT_GPIO1 | DV_INT_GPIO2 | DV_INT_GPIO3 |
-		DV_INT_I2C | DV_INT_SPI | DV_INT_PCM | DV_INT_UART,
-	DV_INT_TIMER | DV_INT_MAILBOX | DV_INT_DOORBELL0 | DV_INT_DOORBELL1 |
-		DV_INT_GPU0HALT | DV_INT_GPU1HALT | DV_INT_ILLEGAL0 | DV_INT_ILLEGAL1
+#if DV_SUPPORT_INTLEVEL
+dv_intlevel_t dv_bcm2835_irq_level[dv_n_bcm2835_iid];
+dv_intlevel_t dv_currentlocklevel;
+dv_bcm2835_imask_t dv_bcm2835_irq_enabled;
+dv_bcm2835_imask_t dv_bcm2835_levelmasks[9];
+#else
+static const dv_bcm2835_imask_t known_irq =
+{	{
+		[0] =	DV_INT_SYST_CM1 |
+				DV_INT_SYST_CM3 |
+				DV_INT_AUX,
+
+		[1] =	DV_INT_I2CSPI_SLV |
+				DV_INT_PWA0 |
+				DV_INT_PWA1 |
+				DV_INT_SMI |
+				DV_INT_GPIO0 |
+				DV_INT_GPIO1 |
+				DV_INT_GPIO2 |
+				DV_INT_GPIO3 |
+				DV_INT_I2C |
+				DV_INT_SPI |
+				DV_INT_PCM |
+				DV_INT_UART,
+
+		[2] =	DV_INT_TIMER |
+				DV_INT_MAILBOX |
+				DV_INT_DOORBELL0 |
+				DV_INT_DOORBELL1 |
+				DV_INT_GPU0HALT |
+				DV_INT_GPU1HALT |
+				DV_INT_ILLEGAL0 |
+				DV_INT_ILLEGAL1
+	}
 };
+#endif
 
 #ifndef DV_CFG_BCM2835_VECTOR_START
 #define DV_CFG_BCM2835_VECTOR_START	0
 #endif
 
-void dv_bcm2835_interrupt_handler(dv_kernel_t *kvars)
+#ifdef DV_DAVROSKA
+void dv_bcm2835_interrupt_handler(void)
 {
-	dv_u32_t pend[3];	/* 2 = basic, 0 = IRQ0, 1 = IRQ1. This order makes enable/disable easier. */
+	dv_bcm2835_imask_t pending;
 	int i, idx, vecidx;
 	dv_u32_t mask;
 
-	/* Only consider the IRQs that are documented. There are others that appear to be used
- 	 * by the GPU. At least one (basic_irq 0x100) seems to follow the CM1 IRQ :-?
+	/* Only consider interrupts that are enabled
 	*/
-	pend[0] = dv_arm_bcm2835_interruptcontroller.irq_pending[0];
-	pend[1] = dv_arm_bcm2835_interruptcontroller.irq_pending[1];
-	pend[2] = dv_arm_bcm2835_interruptcontroller.basic_pending;
+	pending.mask[0] = dv_arm_bcm2835_interruptcontroller.irq_pending[0]	& dv_bcm2835_irq_enabled.mask[0];
+	pending.mask[1] = dv_arm_bcm2835_interruptcontroller.irq_pending[1]	& dv_bcm2835_irq_enabled.mask[1];
+	pending.mask[2] = dv_arm_bcm2835_interruptcontroller.basic_pending	& dv_bcm2835_irq_enabled.mask[2];
 
 	DV_DBG(dv_kprintf("dv_bcm2835_interrupt_handler() called\n"));
-	DV_DBG(dv_kprintf(" -- pend[0] = 0x%08x\n", pend[0]));
-	DV_DBG(dv_kprintf(" -- pend[1] = 0x%08x\n", pend[1]));
-	DV_DBG(dv_kprintf(" -- pend[2] = 0x%08x\n", pend[2]));
-
-	/* Disable all the interrupts that are pending */
-	dv_arm_bcm2835_interruptcontroller.irq_disable[0] = pend[0];
-	dv_arm_bcm2835_interruptcontroller.irq_disable[1] = pend[1];
-	dv_arm_bcm2835_interruptcontroller.irq_disable[2] = pend[2];
+	DV_DBG(dv_kprintf(" -- pend[0] = 0x%08x\n", pending.mask[0]));
+	DV_DBG(dv_kprintf(" -- pend[1] = 0x%08x\n", pending.mask[1]));
+	DV_DBG(dv_kprintf(" -- pend[2] = 0x%08x\n", pending.mask[2]));
 
 	/* Loop over all interrupt sources. Clear and handle the configured sources.
 	*/
@@ -125,9 +121,9 @@ void dv_bcm2835_interrupt_handler(dv_kernel_t *kvars)
 	{
 		idx =  dv_bcm2835_irq_list[i].idx;
 		mask = dv_bcm2835_irq_list[i].mask;
-		if ( pend[idx] & mask )
+		if ( pending.mask[idx] & mask )
 		{
-			pend[idx] &= ~mask;
+			pending.mask[idx] &= ~mask;
 			vecidx = DV_CFG_BCM2835_VECTOR_START+i;
 			(*dv_softvector[vecidx].handler)(kvars, dv_softvector[vecidx].parameter);
 		}
@@ -135,12 +131,85 @@ void dv_bcm2835_interrupt_handler(dv_kernel_t *kvars)
 
 	/* Report unknown interrupts.
 	*/
-	pend[0] &= known_irq[0];
-	pend[1] &= known_irq[1];
-	pend[2] &= known_irq[2];
-	if ( (pend[0] | pend[1] | pend[2]) != 0)
+	pending.mask[0] &= known_irq.mask[0];
+	pending.mask[1] &= known_irq.mask[1];
+	pending.mask[2] &= known_irq.mask[2];
+	if ( (pending.mask[0] | pending.mask[1] | pending.mask[2]) != 0)
 	{
 		dv_kprintf("dv_bcm2835_interrupt_handler() - unknown interrupts: 0x%08x, 0x%08x, 0x%08x\n",
-					pend[0], pend[1], pend[2]);
+					pending.mask[0], pending.mask[1], pending.mask[2]);
 	}
 }
+#else
+
+/* dv_bcm2835_interrupt_handler() - interrupt handler (davros-3 version)
+ *
+ * The BCM2835 interrupt controller is a very simple unit. There is no vectoring, and
+ * there are no priority levels.
+ *
+ * The first step in this function is to read the pending registers into an array.
+ * This gives us a way of configuring IRQs and handling them;
+ * Each configured IRQ has an index and a mask. When the software accepts the
+ * interrupt it clears the pending bit in the array and disables the interrupt.
+ *
+ * After processing all the known IRQs, if there are any left they are reported as unknown
+ * and disabled.
+ *
+ * Interrupts are handled by calling the function from the corresponding
+ * entry in the software vector table. The standard interrupt dispatcher cannot be used because
+ * it is designed to handle a single interrupt, whereas we must handle every interrupt that's
+ * pending. Before calling the handler the source is disabled. This means that if the handler
+ * must re-enable the interrupt if it has been completely handled within the kernel. If the
+ * handler spawns an "ISR" thread, the kernel must re-enable the interrupt source
+ * when the ISR terminates.
+*/
+void dv_bcm2835_interrupt_handler(dv_kernel_t *kvars)
+{
+	dv_bcm2835_imask_t pending;
+	int i, idx, vecidx;
+	dv_u32_t mask;
+
+	/* Only consider the IRQs that are documented. There are others that appear to be used
+ 	 * by the GPU. At least one (basic_irq 0x100) seems to follow the CM1 IRQ :-?
+	*/
+	pending.mask[0] = dv_arm_bcm2835_interruptcontroller.irq_pending[0];
+	pending.mask[1] = dv_arm_bcm2835_interruptcontroller.irq_pending[1];
+	pending.mask[2] = dv_arm_bcm2835_interruptcontroller.basic_pending;
+
+	DV_DBG(dv_kprintf("dv_bcm2835_interrupt_handler() called\n"));
+	DV_DBG(dv_kprintf(" -- pend[0] = 0x%08x\n", pending.mask[0]));
+	DV_DBG(dv_kprintf(" -- pend[1] = 0x%08x\n", pending.mask[1]));
+	DV_DBG(dv_kprintf(" -- pend[2] = 0x%08x\n", pending.mask[2]));
+
+
+	/* Disable all the interrupts that are pending */
+	dv_arm_bcm2835_interruptcontroller.irq_disable[0] = pending.mask[0];
+	dv_arm_bcm2835_interruptcontroller.irq_disable[1] = pending.mask[1];
+	dv_arm_bcm2835_interruptcontroller.irq_disable[2] = pending.mask[2];
+
+	/* Loop over all interrupt sources. Clear and handle the configured sources.
+	*/
+	for ( i = 0; i < dv_n_bcm2835_iid; i++ )
+	{
+		idx =  dv_bcm2835_irq_list[i].idx;
+		mask = dv_bcm2835_irq_list[i].mask;
+		if ( pending.mask[idx] & mask )
+		{
+			pending.mask[idx] &= ~mask;
+			vecidx = DV_CFG_BCM2835_VECTOR_START+i;
+			(*dv_softvector[vecidx].handler)(kvars, dv_softvector[vecidx].parameter);
+		}
+	}
+
+	/* Report unknown interrupts.
+	*/
+	pending.mask[0] &= known_irq.mask[0];
+	pending.mask[1] &= known_irq.mask[1];
+	pending.mask[2] &= known_irq.mask[2];
+	if ( (pending.mask[0] | pending.mask[1] | pending.mask[2]) != 0)
+	{
+		dv_kprintf("dv_bcm2835_interrupt_handler() - unknown interrupts: 0x%08x, 0x%08x, 0x%08x\n",
+					pending.mask[0], pending.mask[1], pending.mask[2]);
+	}
+}
+#endif
