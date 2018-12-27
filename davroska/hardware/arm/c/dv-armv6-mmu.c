2afce5acd24aa373bc4a8eab6d0385cf56ed91d8
David Haworth <dh@thelancashireman.org>
2018-12-27 14:52:03 +0100

    Remove (make optional) debug code

--- davroska/hardware/arm/c/dv-armv6-mmu.c
+++ davroska/hardware/arm/c/dv-armv6-mmu.c
@@ -23,6 +23,10 @@
 #include <dv-armv6-mmu.h>
 #include <dv-arm-cp15.h>
 
+#ifndef DV_DEBUG
+#define DV_DEBUG	0
+#endif
+
 /* davroska doesn't support memory protection or virtualized addresses.
  * So the MMU setup is fixed at startup
  *
@@ -41,7 +45,9 @@ extern dv_u32_t dv_peripheral_end;
 
 void dv_armv6_mmu_setup(void)
 {
+#if DV_DEBUG
 	dv_printf("dv_armv6_mmu_setup() - initialize page table\n");
+#endif
 	for ( int i = 0; i < DV_ARMV6MMU_L1_SIZE; i++ )
 		dv_pagetable.l1page[i] = 0;
 
@@ -55,10 +61,14 @@ void dv_armv6_mmu_setup(void)
 	/* Map the memory pages:
 	 *	
 	*/
+#if DV_DEBUG
 	dv_printf("dv_armv6_mmu_setup() - map the memory sections\n");
+#endif
 	for ( dv_u32_t addr = 0; addr < addr_end_mem; addr += MiB )
 	{
+#if DV_DEBUG
 		dv_printf("dv_armv6_mmu_setup() - memory page %d at 0x%08x\n", page, addr);
+#endif
 		dv_pagetable.l1page[page] = DV_V6MMUL1_SEC |			/* 1 MiB section */
 									DV_V6MMUL1_S_AP_UNO | 		/* Priv. rwx, user no access (APX=0, XN=0) */
 									DV_V6MMUL1_ATTR_MEMORY |	/* Normal memory (TEX_1, B, C, S=0) */
@@ -73,10 +83,14 @@ void dv_armv6_mmu_setup(void)
 
 	/* Map the peripheral pages
 	*/
+#if DV_DEBUG
 	dv_printf("dv_armv6_mmu_setup() - map the peripheral sections - page = %d\n", page);
+#endif
 	for ( dv_u32_t addr = addr_start_per; addr < addr_end_per; addr += MiB )
 	{
+#if DV_DEBUG
 		dv_printf("dv_armv6_mmu_setup() - peripheral page %d at 0x%08x\n", page, addr);
+#endif
 		dv_pagetable.l1page[page] = DV_V6MMUL1_SEC |			/* 1 MiB section */
 									DV_V6MMUL1_S_AP_UNO | 		/* Priv. rwx, user no access (APX=0, XN=0) */
 									DV_V6MMUL1_ATTR_SH_DEV |	/* Shared device (TEX_0, B, C=0, S=0) */
@@ -87,10 +101,16 @@ void dv_armv6_mmu_setup(void)
 
 	/* Caches shouldn't be enabled yet, so no need to flush/invalidate here
 	*/
+#if DV_DEBUG
 	dv_printf("dv_armv6_mmu_setup() -  set TTBR0\n");
+#endif
 	dv_set_translation_table_base_0((dv_u32_t)&dv_pagetable | DV_TTB_C | DV_TTB_RGN_WB);
+#if DV_DEBUG
 	dv_printf("dv_armv6_mmu_setup() -  set domain access control\n");
+#endif
 	dv_set_domain_access_control(0x55555555);       /* Everything to "client" */
+#if DV_DEBUG
 	dv_printf("dv_armv6_mmu_setup() -  enable MMU\n");
+#endif
 	dv_write_cp15_control(dv_read_cp15_control() | DV_CP15_CTRL_M | DV_CP15_CTRL_XP);
 }
