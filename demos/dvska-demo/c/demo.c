feb2e77dfb6e929625b1a47f28cb0a8c4980131d
Haworth David <david.haworth@elektrobit.com>
2019-02-22 14:00:34 +0100

    Adapt davroska demo's Makefile and demo.c to be a bit more "user friendly"

--- demos/dvska-demo/c/demo.c
+++ demos/dvska-demo/c/demo.c
@@ -78,9 +78,9 @@ void main_Led(void)
 				cc[i] = ledstate[i] ? '*' : '-';
 			}
 
-#if 0
+#if DEMO_BOARD==DEMO_PI_ZERO
 			dv_printf("    %c %c %c %c\r", cc[3], cc[2], cc[1], cc[0]);
-#elif 1
+#elif DEMO_BOARD==DEMO_PI3_ARM64
 			extern volatile char core_state[];
 			dv_printf("    %c %c %c %c %c %c %c\r", cc[3], cc[2], cc[1], cc[0],
 													core_state[1], core_state[2], core_state[3]);
