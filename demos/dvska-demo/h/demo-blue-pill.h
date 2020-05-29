569d6e3ba1fc0fc27b8a9fef11fd521e744d4f0d
David Haworth <dh@thelancashireman.org>
2020-05-29 16:29:42 +0200

    Add timer initialization and update

--- demos/dvska-demo/h/demo-blue-pill.h
+++ demos/dvska-demo/h/demo-blue-pill.h
@@ -25,17 +25,26 @@
 
 #include <dv-stm32-gpio.h>
 #include <dv-stm32-uart.h>
+#include <dv-stm32-timx.h>
 #include <dv-nvic.h>
 #include <dv-stm32-interrupts.h>
 
 extern void sysinfo(void);
+extern void dumpPstack(void);
+
+#define panic_info() \
+	do {				\
+		sysinfo();		\
+		dumpPstack();	\
+	} while (0)
 
 #define hw_UartInterruptId		dv_irq_usart1
-#define hw_TimerInterruptId		0
+#define hw_TimerInterruptId		dv_irq_tim2
 
 static inline void hw_ClearTimer(void)
 {
-	/* TODO: to do */
+	dv_tim2.ccr[3] += 1000;			/* No in-the-past check */
+	dv_tim2.sr &= ~DV_TIM_CC4IF;	/* Clear the match flag */
 }
 
 static inline void hw_SetLed(int i, dv_boolean_t state)
@@ -51,7 +60,35 @@ static inline void hw_EnableUartRxInterrupt(void)
 
 static inline void hw_InitialiseMillisecondTicker(void)
 {
-	/* TODO: to do */
+	/* Set the counter running with a 1 us tick
+	*/
+	dv_stm32_timx_init(2, 72);
+
+	/* Enable CC4 interrupt
+	*/
+	dv_tim2.dier |= DV_TIM_CC4IE;
+
+	/* Set the compare mode:
+	 *	- CC4S = 0	--> compare mode
+	 *	- OC4CE = 0	--> ETRF has no effect
+	 *	- OC4M = 0	--> compare has no effect on output
+	 *	- OC4PE = 0	--> CCR4 is not buffered, can be written any time
+	 *	- OC4FE = 0	--> normal trigger effect
+	*/
+	dv_tim2.ccmr[1] = (dv_tim2.ccmr[1] & 0xff) | ( (0) << 8 );
+
+	/* Set the compare register: initial delay of 1 ms
+	*/
+	dv_tim2.ccr[3] = dv_tim2.cnt + 1000;
+
+	/* Enable the comparison operation
+	 *	- clear the match flag
+	 *	- enable event generation
+	 *	- enable the compare function
+	*/
+	dv_tim2.sr &= ~DV_TIM_CC4IF;
+	dv_tim2.egr |= DV_TIM_CC4G;
+	dv_tim2.ccer |= DV_TIM_CCxE << 12;
 }
 
 #endif
