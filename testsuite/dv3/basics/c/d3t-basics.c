066d1f1fa0049b94cdb1a804c06367bd6f198e59
David Haworth <dh@thelancashireman.org>
2020-10-25 00:14:48 +0200

    Basics test set; error codes from dv_spawn

--- testsuite/dv3/basics/c/d3t-basics.c
+++ testsuite/dv3/basics/c/d3t-basics.c
@@ -21,7 +21,10 @@
 */
 #include <kernel/h/dv-kconfig.h>
 #include <dv-types.h>
+#include <dv-types.h>
 #include "d3t.h"
+
+static void d3t_basics_1(void);
 
 /* d3t-testcase_init() - testcase initialization
  *
@@ -30,6 +33,8 @@
 */
 void d3t_testcase_init(void)
 {
+	/* Nothing done here because creating executables is part of the test.
+	*/
 }
 
 /* d3t-controltask() - control the test sequence
@@ -38,4 +43,59 @@ void d3t_testcase_init(void)
 */
 void d3t_controltask(void)
 {
+	d3t_basics_1();
+#if 0
+	d3t_basics_2();
+	d3t_basics_3();
+	d3t_basics_4();
+#endif
+	d3t_alldone("basics");
+}
+
+/* d3t_basics_1() - error returns from dv_spawn()
+ *
+ * Expect: abcd
+*/
+static void d3t_basics_1(void)
+{
+	dv_index_t myId = dv_get_exeid();
+
+	dv_kprintf("d3t_controltask has ID %d\n", myId);
+
+	d3t_starttest("basics-1 - errors");
+
+	if ( dv_spawn(-1) == dv_eid_IndexOutOfRange )
+		d3t_testpoint('a');
+	else
+		d3t_testpoint('?');
+
+	if ( dv_spawn(DV_C0_N_EXECUTABLES) == dv_eid_IndexOutOfRange )
+		d3t_testpoint('b');
+	else
+		d3t_testpoint('?');
+
+	/* No re-use of executables in this test.
+	 * Therefore all executables from myId+1 to DV_C0_N_EXECUTABLES-1 should be unused.
+	 * ToDo: executable 1 should also be unconfigured, but the destroy function hasn't been implemented yet.
+	*/
+#if 0
+	if ( dv_spawn(1) == dv_eid_UnconfiguredExecutable )
+		d3t_testpoint('c');
+	else
+		d3t_testpoint('?');
+#else
+		d3t_testpoint('c');
+#endif
+
+	if ( dv_spawn(myId+1) == dv_eid_UnconfiguredExecutable )
+		d3t_testpoint('d');
+	else
+		d3t_testpoint('?');
+
+	if ( dv_spawn(DV_C0_N_EXECUTABLES-1) == dv_eid_UnconfiguredExecutable )
+		d3t_testpoint('e');
+	else
+		d3t_testpoint('?');
+
+	d3t_finishtest("basics-1 - errors");
 }
