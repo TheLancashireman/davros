4c9f4698152233f5a80c471b684882bbf88117e4
David Haworth <dh@thelancashireman.org>
2020-10-24 09:59:52 +0200

    Empty basics test compiles and links without errors

---
+++ testsuite/dv3/basics/c/d3t-basics.c
@@ -0,0 +1,41 @@
+/* d3t-basics.c - davros-3 test suite : basics
+ *
+ * Copyright David Haworth
+ *
+ * This file is part of davros.
+ *
+ * davros is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * davros is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with davros.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+*/
+#include <kernel/h/dv-kconfig.h>
+#include <dv-types.h>
+#include "d3t.h"
+
+/* d3t-testcase_init() - testcase initialization
+ *
+ *	- Called from d3t_init
+ *	- Create the davros objects that the test needs
+*/
+void d3t_testcase_init(void)
+{
+}
+
+/* d3t-controltask() - control the test sequence
+ *
+ *	- Started as a task after d3t_init terminates
+*/
+void d3t_controltask(void)
+{
+}
