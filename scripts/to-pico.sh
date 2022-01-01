2c712d56901387d8720b8b9b2f49b5db4ad575e4
David Haworth <dh@thelancashireman.org>
2022-01-01 12:04:44 +0100

    Change to-pico.sh to avoid needing text file

--- scripts/to-pico.sh
+++ scripts/to-pico.sh
@@ -18,28 +18,57 @@ PICOMNT=/media/pico
 set -e
 
 if [ -z "$1" ] ; then
-	UF2FILE=bin/davroska.uf2
+	UF2FILE="bin/davroska.uf2"
 else
-	UF2FILE=$1
+	UF2FILE="$1"
 fi
 
-if [ -e ${UF2FILE} ]; then
+if [ -e "${UF2FILE}" ]; then
 	echo "Uploading ${UF2FILE}"
 else
 	echo "Usage: to-pico.sh <UF2-file>"
 	exit 1
 fi
 
-sudo mount -o uid=${USER} ${PICODEV} ${PICOMNT}
-if [ -e ${PICOMNT}/INFO_UF2.TXT ]; then
-	if diff ${PICOMNT}/INFO_UF2.TXT INFO_UF2.TXT ; then
-		cp ${UF2FILE} ${PICOMNT}
+sudo mount -o uid="${USER}" "${PICODEV}" "${PICOMNT}"
+if [ -e "${PICOMNT}/INFO_UF2.TXT" ]; then
+	LINE=A
+	CHECK=OK
+	while read -r l ; do
+		case ${LINE} in
+		A)	if [ "${l}" != "UF2 Bootloader v2.0" ] ; then
+				echo "Unexpected line 1 in INFO_UF2.TXT"
+				CHECK=BAD
+			fi
+			LINE=B
+			;;
+		B)	if [ "${l}" != "Model: Raspberry Pi RP2" ] ; then
+				echo "Unexpected line 2 in INFO_UF2.TXT"
+				CHECK=BAD
+			fi
+			LINE=C
+			;;
+		C)	if [ "${l}" != "Board-ID: RPI-RP2" ] ; then
+				echo "Unexpected line 3 in INFO_UF2.TXT"
+				CHECK=BAD
+			fi
+			LINE=D
+			;;
+		D)	echo "Unexpected extra line in INFO_UF2.TXT"
+			CHECK=BAD
+			LINE=E
+			;;
+		E)
+			;;
+		esac
+	done < ${PICOMNT}/INFO_UF2.TXT
+
+	if [ "${LINE}" = "D" ] && [ "${CHECK}" = "OK" ] ; then
+		cp "${UF2FILE}" "${PICOMNT}"
 	else
 		echo "The mounted UF2 bootloader is not a Raspberry Pi pico"
-		exit 1
 	fi
 else
 	echo "The mounted filesystem is not a UF2 bootloader"
-	exit 1
 fi
-sudo umount ${PICOMNT}
+sudo umount "${PICOMNT}"
