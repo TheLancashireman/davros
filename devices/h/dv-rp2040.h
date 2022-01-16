724c7aa14fbb2b722b5fb4254f38cdc0414eadbf
David Haworth <dh@thelancashireman.org>
2022-01-16 16:16:43 +0100

    Add definition for sysinfo peripheral to get chip id

--- devices/h/dv-rp2040.h
+++ devices/h/dv-rp2040.h
@@ -26,4 +26,22 @@
 #define	DV_OFFSET_W1S	0x2000		/* Write 1 to set */
 #define	DV_OFFSET_W1C	0x3000		/* Write 1 to clear */
 
+typedef struct dv_rp2040_sysinfo_s dv_rp2040_sysinfo_t;
+
+struct dv_rp2040_sysinfo_s
+{
+	dv_u32_t	chip_id;		/* 0x00 - JEDEC jep-106 compliant chip identifier */
+	dv_u32_t	platform;		/* 0x04 - platform (ASIC or FPGA) */
+	dv_u32_t	gap[14];
+	dv_u32_t	gitref_rp2040;	/* 0x40 - git hash of chip source */
+};
+
+#define DV_RP2040_SYSINFO_BASE	0x40000000
+
+#define DV_RP2040_MANUFACTURER	0x00000fff
+#define DV_RP2040_PART			0x0ffff000
+#define DV_RP2040_REVISION		0xf0000000
+
+#define dv_rp2040_sysinfo		(((dv_rp2040_sysinfo_t *)DV_RP2040_SYSINFO_BASE)[0])
+
 #endif
