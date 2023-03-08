3eb6f4b82583d03af461e8411fe887847834b44a
David Haworth <dh@thelancashireman.org>
2023-03-08 23:01:17 +0100

    Sanitise the obj-dir; clean for shellcheck

--- scripts/tusb-list.sh
+++ scripts/tusb-list.sh
@@ -2,5 +2,12 @@
 # Generate a list of all the TinyUSB object files
 #
 # Called from the Makefile
-cd $1
-find src -name \*.c -exec basename \{\} \; | sed -e 's/\.c$/\.o/' | sed -e "s/^/$2\//"
+#
+# $1 is the tinyusb directory
+# $2 is the object directory
+
+# Sanitise the object directory - add \ in front of /
+O=$(echo "$2" | sed -e 's/\//\\\//g')
+
+cd "$1" || exit
+find src -name \*.c -exec basename \{\} \; | sed -e 's/\.c$/\.o/' | sed -e "s/^/${O}\//"
